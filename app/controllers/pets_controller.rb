02380de269a7231814a5bf463b5b72be98f3042c
Ashley Wong <ashwong04@gmail.com>
2019-03-07 23:21:10 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -35,13 +35,11 @@ class PetsController < ApplicationController
     if !params[:pet].keys.include?("owner_id")
       params[:pet]["owner_id"] = ''
     end
-    
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
-    
     redirect to "pets/#{@pet.id}"
   end
   
