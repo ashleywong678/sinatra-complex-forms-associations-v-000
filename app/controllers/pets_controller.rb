518a2cf39f980c4aca014e36bcc6bb9d4c661bac
Ashley Wong <ashwong04@gmail.com>
2019-03-07 23:24:43 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -32,6 +32,7 @@ class PetsController < ApplicationController
   end
   
   patch '/pets/:id' do
+    binding.pry
     if !params[:pet].keys.include?("owner_id")
       params[:pet]["owner_id"] = ''
     end
